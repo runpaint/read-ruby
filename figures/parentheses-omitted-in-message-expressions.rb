2e8db32957a22d95ad059a38d17e4f3af250c684
Run Paint Run Run <runrun@runpaint.org>
2009-11-17 11:56:57 +0000

    Add example of omitting parentheses in message expressions

---
+++ figures/parentheses-omitted-in-message-expressions.rb
@@ -0,0 +1,4 @@
+"spending".sub(/spend/, 'sav') #=> "saving"
+"spending".sub /spend/, 'sav'  #=> "saving"
+";".gsub(/(.)/, '.' => 'full stop', ',' => 'comma', ';' => 'semicolon') #=> "semicolon"
+";".gsub /(.)/, '.' => 'full stop', ',' => 'comma', ';' => 'semicolon' #=> "semicolon"
