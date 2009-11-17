f1e9a0ee88149d7621115e697fde60df76e772b3
Run Paint Run Run <runrun@runpaint.org>
2009-11-17 13:29:44 +0000

    Add example of illegal omission of parentheses in msg exp.

---
+++ figures/message-expressions-omit-parentheses-syntaxerror.rb
@@ -0,0 +1,6 @@
+cubes = [1.0]
+cubes.push cubes.first * 8, Math.log2 134217728, 64.0
+#=> SyntaxError: syntax error, unexpected tINTEGER, expecting $end
+#   cubes.push cubes.first * 8, Math.log2 134217728, 64.0
+#                                                  ^
+cubes.push cubes.first * 8, Math.log2(134217728), 64.0 #=> [1.0, 8.0, 27.0, 64.0]
