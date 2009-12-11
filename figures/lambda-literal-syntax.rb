2dcf9019f46499b707e579e70e4dd523e8455c09
Run Paint Run Run <runrun@runpaint.org>
2009-12-11 07:36:27 +0000

    Closures: Tweak example

--- figures/lambda-literal-syntax.rb
+++ figures/lambda-literal-syntax.rb
@@ -2,7 +2,7 @@ require 'resolv'
 ->(host) do 
   Resolv.getaddress host
 end.call 'runpaint.org' #=> "208.94.116.80"
-resolv = ->(ips) { Hash[ ips.map {|n| [n, Resolv.getname(n)]} ] }
+resolv = ->(ips) { Hash[ ips.map {|ip| [ip, Resolv.getname(ip)]} ] }
 resolv[%w{128.9.160.27 66.80.146.7 65.181.149.201 217.147.82.116}] 
 #=> {"128.9.160.27"  =>"www.rfc-editor.org", "66.80.146.7"   =>"forced.attrition.org",
 #=>  "65.181.149.201"=>"ycombinator.com",    "217.147.82.116"=>"ns.irational.org"}
