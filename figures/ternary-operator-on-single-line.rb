308adccf5583823734f650e45d81a4f6109f8203
Run Paint Run Run <runrun@runpaint.org>
2010-02-10 03:01:49 +0000

    Flow: Tweak formatting of example

--- figures/ternary-operator-on-single-line.rb
+++ figures/ternary-operator-on-single-line.rb
@@ -1,6 +1,6 @@
 VOWELS = %w{a e i o u}
 ['d', 'e'].each do |letter|
-  puts "#{letter} is a #{VOWELS.include?(letter) ? "vowel" : "consonant"}"
+  puts "#{letter} is a %s" % (VOWELS.include?(letter) ? "vowel" : "consonant")
 end
 #=> "d is a consonant"
 #=> "e is a vowel"
