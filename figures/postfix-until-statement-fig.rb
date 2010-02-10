8a45bdb6a61dcc07e65c0272d3159a6d71df6b75
Run Paint Run Run <runrun@runpaint.org>
2010-02-10 02:34:24 +0000

    Flow: Fix typo in example

--- figures/postfix-until-statement-fig.rb
+++ figures/postfix-until-statement-fig.rb
@@ -6,6 +6,6 @@ class Integer
     n
   end
 end
-1.upto(10).map {|n| {n, n.next_prime}}
+1.upto(10).map {|n| {n => n.next_prime}}
 #=> [{1=>2}, {2=>3},  {3=>5},  {4=>5},  {5=>7}, 
 #    {6=>7}, {7=>11}, {8=>11}, {9=>11}, {10=>11}]
