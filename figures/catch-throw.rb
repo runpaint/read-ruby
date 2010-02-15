40e3820cb1dffbe31dbeb4f5ba4e274dc4a0a038
Run Paint Run Run <runrun@runpaint.org>
2010-02-15 04:03:57 +0000

    Flow: Add example for catch/throw

---
+++ figures/catch-throw.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+
+def follow(url)
+  chain = []
+  while url do
+    url = catch(:redirect) do
+      chain << url = URI.parse(url)
+      resp = Net::HTTP.new(url.host).request(Net::HTTP::Get.new url.path)
+      case resp.code.to_i
+        when 200       then return chain.join(' => ')
+        when 300...400 then throw :redirect, resp['location']
+        else raise "#{url} => #{resp.code}: #{resp.message}"
+      end
+    end
+  end
+end
+follow('http://w3c.org/html') 
+#=> "http://w3c.org/html => http://www.w3.org/html => http://www.w3.org/html/"
