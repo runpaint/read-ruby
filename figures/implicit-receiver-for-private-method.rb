5691cf4d904d62f8d47c308bb218821dc5d9ac0d
Run Paint Run Run <runrun@runpaint.org>
2010-03-06 20:55:38 +0000

    Messages: Fix figure layout

--- figures/implicit-receiver-for-private-method.rb
+++ figures/implicit-receiver-for-private-method.rb
@@ -4,18 +4,21 @@ class Schneier
     "Teach a man to fish..."
   end
 end
-Schneier.new.blowfish #=> NoMethodError: private method `blowfish' called for #<Schneier:0x8b1d458>
+Schneier.new.blowfish
+#=> NoMethodError: private method `blowfish' called for #<Schneier:0x8b1d458>
 
 class Schneier
   def backdoor
     self.blowfish
   end
 end
-Schneier.new.backdoor #=> NoMethodError: private method `blowfish' called for #<Schneier:0x8c8b204>
+Schneier.new.backdoor
+#=> NoMethodError: private method `blowfish' called for #<Schneier:0x8c8b204>
 
 class Schneier
   def backdoor
     blowfish
   end
 end
-Schneier.new.backdoor #=> "Teach a man to fish..."
+Schneier.new.backdoor 
+#=> "Teach a man to fish..."
