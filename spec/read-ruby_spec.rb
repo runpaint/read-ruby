b747e7e789dcf78a2310b2ec0c2a59dc767216f2
Run Paint Run Run <runrun@runpaint.org>
2010-03-08 03:27:00 +0000

    Specs: Factor out common examples into shared spec

--- spec/read-ruby_spec.rb
+++ spec/read-ruby_spec.rb
@@ -1,4 +1,24 @@
 require_relative './spec_helper'
+
+shared_examples_for 'Root page' do
+
+  it "should feature the book title" do
+    @page.body.should =~ /Read Ruby/
+  end
+
+  it "should have a Content-Type header with 'text/html' as the value" do
+    @page.response['content-type'].should be_start_with('text/html')
+  end
+
+  it "should have a Content-Type header with UTF-8 as the character set" do
+    @page.response['content-type'].should =~ /charset=UTF-8/i
+  end
+
+  it "should have a Content-Language header with 'en-GB' as the value" do
+    @page.response['content-language'].should == 'en-GB'
+  end
+end
+
 describe "Root page" do
   before(:all) do
     @url = 'http://ruby.runpaint.org/'
@@ -9,22 +29,8 @@ describe "Root page" do
     before(:all) do
       @page = @ua.new.get @url
     end
-
-    it "should feature the book title" do
-      @page.body.should =~ /Read Ruby/
-    end
-
-    it "should have a Content-Type header with 'text/html' as the value" do
-      @page.response['content-type'].should be_start_with('text/html')
-    end
-
-    it "should have a Content-Type header with UTF-8 as the character set" do
-      @page.response['content-type'].should =~ /charset=UTF-8/i
-    end
-
-    it "should have a Content-Language header with 'en-GB' as the value" do
-      @page.response['content-language'].should == 'en-GB'
-    end
+    
+    it_should_behave_like 'Root page'
 
     it "should have a Content-Encoding header with the value 'gzip'" do
       @page.response['content-encoding'].should == 'gzip'
@@ -45,21 +51,7 @@ describe "Root page" do
       @page = @ua.new.tap{|m| m.gzip_enabled = false}.get @url
     end
 
-    it "should feature the book title" do
-      @page.body.should =~ /Read Ruby/
-    end
-
-    it "should have a Content-Type of text/html" do
-      @page.response['content-type'].should be_start_with('text/html')
-    end
-
-    it "should have a Content-Type charset of UTF-8" do
-      @page.response['content-type'].should =~ /charset=UTF-8/i
-    end
-
-    it "should have a Content-Language of en-GB" do
-      @page.response['content-language'].should == 'en-GB'
-    end
+    it_should_behave_like 'Root page'
 
     it "should not have a Content-Encoding header" do
       @page.response.should_not have_key('content-encoding')
@@ -67,11 +59,11 @@ describe "Root page" do
 
     it "should not be gzipped" do
       raw = open(@url)
-      body = raw.dup.read
+      raw.read.should =~ /Read Ruby/
+      raw.rewind
       -> do
         Zlib::GzipReader.new(raw).read
       end.should raise_error(Zlib::Error)
-      body.should =~ /Read Ruby/
     end
   end
 end
