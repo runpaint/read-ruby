74d307be4a76da05b898de7e34e738b4dc4506fe
Run Paint Run Run <runrun@runpaint.org>
2010-03-08 07:51:33 +0000

    Spec: Check internal links from root page

--- spec/read-ruby_spec.rb
+++ spec/read-ruby_spec.rb
@@ -2,6 +2,10 @@ require_relative './spec_helper'
 
 shared_examples_for 'Root page' do
 
+  def internal_links
+    @page.links_with(:href => %r{^(#|/)[^/]}).map{|u| URI.join(@url,u.href)}
+  end
+
   it "should feature the book title" do
     @page.body.should =~ /Read Ruby/
   end
@@ -17,17 +21,28 @@ shared_examples_for 'Root page' do
   it "should have a Content-Language header with 'en-GB' as the value" do
     @page.response['content-language'].should == 'en-GB'
   end
+
+  it "should have internal links" do
+    internal_links.should_not be_empty
+  end
+
+  it "should not have broken internal links" do
+    internal_links.map{|u| u.tap{|_| _.fragment = nil}}.uniq.each do |url|
+      @ua.head(url.to_s).code.to_i.should == 200
+    end
+  end
 end
 
 describe "Root page" do
   before(:all) do
     @url = 'http://ruby.runpaint.org/'
-    @ua = Mechanize
+    @agent = Mechanize
+    @ua = @agent.new
   end
 
   describe " for clients supporting compression" do
     before(:all) do
-      @page = @ua.new.get @url
+      @page = @ua.get @url
     end
     
     it_should_behave_like 'Root page'
@@ -48,7 +63,7 @@ describe "Root page" do
 
   describe " for clients not supporting compression" do
     before(:all) do
-      @page = @ua.new.tap{|m| m.gzip_enabled = false}.get @url
+      @page = @agent.new.tap{|m| m.gzip_enabled = false}.get @url
     end
 
     it_should_behave_like 'Root page'
