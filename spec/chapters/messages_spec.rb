9a588b436970d28488bb53bb4d9dd59cfc57b4ce
Run Paint Run Run <runrun@runpaint.org>
2010-03-09 09:39:15 +0000

    Specs: Add more chapters; make figures optional

--- spec/chapters/messages_spec.rb
+++ spec/chapters/messages_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../spec_helper'
 
-require_relative '../shared/chapter'
+require_relative '../shared/chapter_with_figures'
 
 describe "/messages" do
   before(:all) do
     @chapter = :messages
   end
 
-  it_should_behave_like 'A chapter'
+  it_should_behave_like 'A chapter with figures'
 end
--- spec/shared/chapter_page.rb
+++ spec/shared/chapter_page.rb
@@ -35,26 +35,6 @@ shared_examples_for 'A chapter page' do
     @page.parser.at('meta[charset=utf-8]')['charset'].should == 'utf-8'
   end
 
-  it "should contain multiple figures" do
-    @page.parser.css('figure').size.should > 1
-  end
-
-  it "should contain multiple code samples" do
-    @page.parser.css('figure[id]').
-      select{|f| f['id'].end_with?('.rb')}.
-      size.should > 1
-  end
-
-  it "should link code samples to the corresponding example file" do
-    @page.parser.css('a').each do |a|
-      a.css('figure[id]').
-        select{|figure| figure['id'].end_with?('.rb')}.
-        each do |figure|
-          a['href'].should be_end_with(figure['id'])
-        end
-    end
-  end
-
   it "should contain multiple headed sections" do
     @page.parser.css('section > h1').size.should > 1
   end
