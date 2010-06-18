07ba5e4fb85a1b17659315103d629d06ffcc0c3f
Run Paint Run Run <runrun@runpaint.org>
2010-06-18 07:23:24 +0100

    Text: Explain String comparison

---
+++ examples/string-comparison.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+?a < ?A #=> false
+'pre' < 'prefix' #=> true
+?Β <=> ?β #=> -1
+?α < ?β #=> true
+'11' > '100' #=> true
+?a.casecmp ?A #=> 0
+'epsilON'.casecmp 'Epsilon' #=> 0
+?Σ.casecmp ?ς #=> -1
