a1000c61b2d8fa795cb6662ef45256213e162e54
Run Paint Run Run <runrun@runpaint.org>
2010-07-11 22:29:11 +0100

    Sitemap: Use whitelist instead of blacklist
    The previous approach blacklisted certain file extensions from appearing in
    the sitemap. This was ineffective because it needed updating every time a file
    extension was added. Now, we whitelist extensions to include.

--- lib/read-ruby/sitemap.rb
+++ lib/read-ruby/sitemap.rb
@@ -1,17 +1,17 @@
 class Sitemap < Mustache
   include ReadRuby
 
-  BAD_EXT = %w{.css .js .xml}
+  GOOD_EXT = %w{.html .png .rb}
 
   def urls
     Dir["#{OUT_DIR}/*", 
         "#{Example.dir}/*#{Example.target_ext}", 
         "#{Railroad.dir}/*#{Railroad.target_ext}"
-       ].map{|f| Pathname f}.
-         reject(&:directory?).
-         reject{|f| BAD_EXT.any?{|e| f.extname == e} or f.fnmatch?('*google*')}.
-         reject{|f| Error.dir.join(f.basename).exist? }.
-         map{|f| f.dirname == OUT_DIR ? f.basename : f}.
-         map{|f| {url: ROOT_URL + f.sub_ext('').to_s}}
+    ].map{|f| Pathname f}.
+      select{|f| GOOD_EXT.any?{|e| f.extname == e}}.
+      reject{|f| f.fnmatch?('*google*') or f.basename.to_s == 'index.html'}.
+      reject{|f| Error.dir.join(f.basename).exist? }.
+      map{|f| f.dirname == OUT_DIR ? f.basename : f}.
+      map{|f| {url: ROOT_URL + f.sub_ext('').to_s}}
   end
 end
