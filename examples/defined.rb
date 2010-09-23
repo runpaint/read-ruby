7399338ab0816ef7fc8f91debde7138e07d3e6bb
Run Paint Run Run <runrun@runpaint.org>
2010-09-23 02:29:51 +0100

    Variables: Fix example; closes #42

--- examples/defined.rb
+++ examples/defined.rb
@@ -1,6 +1,6 @@
 defined? $a #=> nil
 defined? 3 + 2 #=> "method"
-var = defined? true and nil #=> "true"
+var = defined? true or nil #=> "true"
 defined? var #=> "local-variable"
 defined? 1.times { :one } #=> "expression"
 defined? yield #=> nil
