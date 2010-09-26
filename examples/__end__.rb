48dcce2a5d36576169614f38b2c0f463372b7e54
Run Paint Run Run <runrun@runpaint.org>
2010-09-26 22:35:43 +0100

    Programs: Document __END__

---
+++ examples/__end__.rb
@@ -0,0 +1,8 @@
+DATA.lines.map{|l| l.split.first}
+#=> ["Lovers", "part", "also", "a", "--"]
+__END__
+Lovers for the most
+part are without hope: passion
+also is just
+a bridge, a means of connection
+-- Marina Ivanovna Tsvetaeva, "The Poem of the End"
