92f75ada49bf815835e40a2b13a75024bb456295
Run Paint Run Run <runrun@runpaint.org>
2010-10-01 13:07:42 +0100

    Arrays: Add examples for 'Element Insertion'

---
+++ examples/array-push-unshift.rb
@@ -0,0 +1,8 @@
+kubrick = []
+kubrick << 'Lolita' #=> ["Lolita"]
+kubrick.push('Dr. Strangelove', 
+             '2001', 
+             'A Clockwork Orange')
+#=> ["Lolita", "Dr. Strangelove", "2001", "A Clockwork Orange"]
+kubrick.unshift 'Spartacus'
+#=> ["Spartacus", "Lolita", "Dr. Strangelove", "2001", "A Clockwork Orange"]
