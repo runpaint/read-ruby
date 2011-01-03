f2edbb8d4d84d3d751b5c074b74c01615e129f73
Run Paint Run Run <runrun@runpaint.org>
2011-01-03 09:55:40 +0000

    Enumerator: Simplify _Instantiation_ section.
    Remove mention of #enum_for and Enumerator.new without a block; suggest #enum_to, instead.

--- examples/enu-enumerator-instantiation.rb
+++ examples/enu-enumerator-instantiation.rb
@@ -1,4 +1,4 @@
-"Once bitten, twice shy".enum_for(:bytes).first(5)
+"Once bitten, twice shy".to_enum(:bytes).first(5)
 #=> [79, 110, 99, 101, 32]
 Enumerator.new do |yielder|
   (1..Float::INFINITY).each do |n|
